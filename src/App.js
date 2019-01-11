da999f464d2676f53559c08aeb7697a9995fcde7
Andrew Brunner <andrew.brunner@froedtert.com>
2019-01-10 18:06:34 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+import { LoginForm } from './components/LoginForm';
+
+
+class App extends Component {
+
+//life cycle method runs before app is rendered
+    componentWillMount () {
+        firebase.initializeApp ({
+            
+            apiKey: "AIzaSyBLNoJR524alAfrZPQ1RsIo0hkdUijMPvA",
+            authDomain: "auth-8e375.firebaseapp.com",
+            databaseURL: "https://auth-8e375.firebaseio.com",
+            projectId: "auth-8e375",
+            storageBucket: "auth-8e375.appspot.com",
+            messagingSenderId: "989364508895"
+              
+        });
+    }
+
+    render () {
+        return (
+            <View>
+                <Header headerText="Authentication"/>
+                <LoginForm />
+            </View>
+        );
+    } 
+}
+
+export default App;
