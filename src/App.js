2a93256b600b7bfd9dff1373a2478247c7517756
Andrew Brunner <andrew.brunner@froedtert.com>
2019-01-22 17:35:03 -0600

    added authentication logic when login button is pressed and a progress spinner and error message.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 
 class App extends Component {
+    state = { loggedIn: null };
 
 //life cycle method runs before app is rendered
     componentWillMount () {
@@ -19,13 +20,36 @@ class App extends Component {
             messagingSenderId: "989364508895"
               
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false});
+            }
+        });
+    }
+
+    renderContent () {
+
+        switch (this.state.loggedIn) {
+            case true:
+                return <Button>Log Out</Button>;
+
+            case false:
+                return <LoginForm />;
+
+            default:
+                return <Spinner size="large" />;
+        }
     }
 
     render () {
         return (
             <View>
                 <Header headerText="Authentication"/>
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
     } 
